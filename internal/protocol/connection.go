8dcfd822f16319e25b19112f439bcd5daa77eea5
Rhisiart <ricardobenfiquista13@gmail.com>
2024-08-15 15:07:08 +0100

    reading and writing from the connection

--- internal/protocol/connection.go
+++ internal/protocol/connection.go
@@ -1,4 +1,36 @@
 package protocol
 
+import "net"
+
 type Connection struct {
+	FrameReader *FrameReader
+	FrameWriter *FrameWriter
+	Id          int
+	Conn        net.Conn
+}
+
+func NewConnection(conn net.Conn, id int) *Connection {
+	return &Connection{
+		FrameReader: NewFrameReader(conn),
+		FrameWriter: NewFrameWriter(conn),
+		Id:          id,
+		Conn:        conn,
+	}
+}
+
+func (conn *Connection) Next() (*Package, error) {
+	frame, err := conn.FrameReader.Read()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var p Package
+	unmarshalErr := p.Unmarshal(frame)
+
+	if unmarshalErr != nil {
+		return nil, unmarshalErr
+	}
+
+	return &p, nil
 }
